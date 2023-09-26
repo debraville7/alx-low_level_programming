754d0ce35ae3a30e2d588816321e10f37806609a
debraville7 <ogbonnadeborah7@gmail.com>
2023-09-25 20:38:16 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -20,25 +20,3 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	}
 	return (dest);
 }
-
-
-2-Strchr.c
-
-#include "main.h"
-/**
- * _strchr - Entry point
- * @s: input
- * @c: input
- * Return: Always 0 (Success)
- */
-char *_strchr(char *s, char c)
-{
-	int i = 0;
-
-	for (; s[i] >= '\0'; i++)
-	{
-		if (s[i] == c)
-			return (&s[i]);
-	}
-	return (0);
-}
